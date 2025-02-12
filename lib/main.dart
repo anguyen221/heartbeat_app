f7f8fef378ddf9417ede11cea7ea8fd2480b3abf
anguyen221 <anguyen221@student.gsu.edu>
2025-02-12 13:41:10 -0500

    Final adjustments and changes

--- lib/main.dart
+++ lib/main.dart
@@ -20,130 +20,8 @@ class HeartbeatScreen extends StatefulWidget {
   _HeartbeatScreenState createState() => _HeartbeatScreenState();
 }
 
-class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    )..repeat(reverse: true);
-
-    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(_controller);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink[50],
-      body: Center(
-        child: ScaleTransition(
-          scale: _animation,
-          child: Icon(
-            Icons.favorite,
-            color: Colors.red,
-            size: 100,
-
-            void main() {
-  runApp(ValentineApp());
-}
-
-class ValentineApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HeartbeatScreen(),
-    );
-  }
-}
-
-class HeartbeatScreen extends StatefulWidget {
-  @override
-  _HeartbeatScreenState createState() => _HeartbeatScreenState();
-}
-
-class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-  Timer? _timer;
-  int _countdown = 10;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    )..repeat(reverse: true);
-
-    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(_controller);
-
-    _startCountdown();
-  }
-
-  void _startCountdown() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      if (_countdown == 0) {
-        _timer?.cancel();
-      } else {
-        setState(() {
-          _countdown--;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink[50],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ScaleTransition(
-            scale: _animation,
-            child: Icon(
-              Icons.favorite,
-              color: Colors.red,
-              size: 100,
-
-              void main() {
-  runApp(ValentineApp());
-}
-
-class ValentineApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HeartbeatScreen(),
-    );
-  }
-}
-
-class HeartbeatScreen extends StatefulWidget {
-  @override
-  _HeartbeatScreenState createState() => _HeartbeatScreenState();
-}
-
-class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProviderStateMixin {
+class _HeartbeatScreenState extends State<HeartbeatScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
   Timer? _timer;
@@ -160,7 +38,6 @@ class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProv
     )..repeat(reverse: true);
 
     _animation = Tween<double>(begin: 1.0, end: 1.2).animate(_controller);
-
     _startCountdown();
   }
 
@@ -213,14 +90,13 @@ class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProv
               duration: Duration(seconds: 2),
               child: Text(
                 _message,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.redAccent),
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            "$_countdown",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent),
+              ),
+            )
+          ],
         ),
       ),
     );
