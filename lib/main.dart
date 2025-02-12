509a0b7e5c71107e75ab5b914e51cdc3ff1e8cf0
panjwaniamaan <panjwaniamaan@gmail.com>
2025-02-12 13:35:01 -0500

    Added countdown timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(ValentineApp());
@@ -51,6 +52,83 @@ class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProv
             Icons.favorite,
             color: Colors.red,
             size: 100,
+
+            void main() {
+  runApp(ValentineApp());
+}
+
+class ValentineApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HeartbeatScreen(),
+    );
+  }
+}
+
+class HeartbeatScreen extends StatefulWidget {
+  @override
+  _HeartbeatScreenState createState() => _HeartbeatScreenState();
+}
+
+class _HeartbeatScreenState extends State<HeartbeatScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  Timer? _timer;
+  int _countdown = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(_controller);
+
+    _startCountdown();
+  }
+
+  void _startCountdown() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_countdown == 0) {
+        _timer?.cancel();
+      } else {
+        setState(() {
+          _countdown--;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[50],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ScaleTransition(
+            scale: _animation,
+            child: Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 100,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "$_countdown",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
       ),
